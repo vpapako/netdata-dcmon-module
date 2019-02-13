3112f53e2c3faa0396df9334c39c1f0471bbfd43
Vassilis Papakonstantinou <vpapako@gmail.com>
2019-02-13 21:19:48 +0200

    define global variables

--- dcmon.chart.sh
+++ dcmon.chart.sh
@@ -12,14 +12,53 @@
 
 # _update_every is a special variable - it holds the number of seconds
 # between the calls of the _update() function
-dcmon_update_every=
+dcmon_update_every=5
 
 # the priority is used to sort the charts on the dashboard
 # 1 = the first chart
-dcmon_priority=
+dcmon_priority=1
 
 # global variables to store our collected data
 # remember: they need to start with the module name
+
+# HVAC Board (watts)
+dcmon_RphaseWatts=
+dcmon_SphaseWatts=
+dcmon_TphaseWatts=
+dcmon_RphaseKiloWattHours=
+dcmon_SphaseKiloWattHours=
+dcmon_TphaseKiloWattHours=
+
+# HVAC Board (amps)
+dcmon_RphaseIrms=
+dcmon_SphaseIrms=
+dcmon_TphaseIrms=
+
+# Board "A" Board (watts)
+dcmon_RphaseWattsA=
+dcmon_SphaseWattsA=
+dcmon_TphaseWattsA=
+dcmon_RphaseKiloWattHoursA=
+dcmon_SphaseKiloWattHoursA=
+dcmon_TphaseKiloWattHoursA=
+
+# Board "A" (amps)
+dcmon_RphaseIrmsA=
+dcmon_SphaseIrmsA=
+dcmon_TphaseIrmsA=
+
+# Board "B" Board (watts)
+dcmon_RphaseWattsB=
+dcmon_SphaseWattsB=
+dcmon_TphaseWattsB=
+dcmon_RphaseKiloWattHoursB=
+dcmon_SphaseKiloWattHoursB=
+dcmon_TphaseKiloWattHoursB=
+
+# Board "B" (amps)
+dcmon_RphaseIrmsB=
+dcmon_SphaseIrmsb=
+dcmon_TphaseIrmsB=
 
 # _check is called once, to find out if this chart should be enabled or not
 dcmon_check() {
