d7acb21badfb42494c972a254efd2f2a243d3bea
Vassilis Papakonstantinou <vpapako@gmail.com>
2019-02-14 07:43:38 +0200

    add chart for HVAC Board in watts

--- dcmon.chart.sh
+++ dcmon.chart.sh
@@ -12,7 +12,7 @@
 
 # _update_every is a special variable - it holds the number of seconds
 # between the calls of the _update() function
-dcmon_update_every=5
+dcmon_update_every=1
 
 # the priority is used to sort the charts on the dashboard
 # 1 = the first chart
@@ -60,6 +60,28 @@ dcmon_RphaseIrmsB=
 dcmon_SphaseIrmsb=
 dcmon_TphaseIrmsB=
 
+
+dcmon_get() {
+        # do all the work to collect / calculate the values
+        # for each dimension
+        #
+        # Remember:
+        # 1. KEEP IT SIMPLE AND SHORT
+        # 2. AVOID FORKS (avoid piping commands)
+        # 3. AVOID CALLING TOO MANY EXTERNAL PROGRAMS
+        # 4. USE LOCAL VARIABLES (global variables may overlap with other modules)
+
+	dcmon_RphaseWatts=$(curl -s "http://dcmon.datacenter.uoc.gr/feed/list.json?userid=1" | jq '. [0] .value' | sed -e 's/["]//g' | awk '{print int($1+0.5)}')
+	dcmon_SphaseWatts=$(curl -s "http://dcmon.datacenter.uoc.gr/feed/list.json?userid=1" | jq '. [1] .value' | sed -e 's/["]//g' | awk '{print int($1+0.5)}')
+	dcmon_TphaseWatts=$(curl -s "http://dcmon.datacenter.uoc.gr/feed/list.json?userid=1" | jq '. [2] .value' | sed -e 's/["]//g' | awk '{print int($1+0.5)}')
+
+        # this should return:
+        #  - 0 to send the data to netdata
+        #  - 1 to report a failure to collect the data
+
+        return 0
+}
+
 # _check is called once, to find out if this chart should be enabled or not
 dcmon_check() {
 	# this should return:
@@ -73,6 +95,14 @@ dcmon_check() {
 
 # _create is called once, to create the charts
 dcmon_create() {
+	# create the HVAC Board (watts) chart with 3 dimensions
+	cat <<EOF
+CHART dcmon.hvac-board-watts '' "Energy consumption of HVAC Board" "watts" $dcmon_priority $dcmon_update_every
+DIMENSION Rphase '' absolute 1 1
+DIMENSION Sphase '' absolute 1 1
+DIMENSION Tphase '' absolute 1 1
+EOF
+
 	return 0
 }
 
@@ -80,5 +110,16 @@ dcmon_create() {
 dcmon_update() {
 	# the first argument to this function is the microseconds since last update
 	# pass this parameter to the BEGIN statement (see bellow).
+
+	dcmon_get || return 1
+
+	# write the result of the work.
+        cat <<VALUESEOF
+BEGIN dcmon.hvac-board-watts $1
+SET Rphase = $dcmon_RphaseWatts
+SET Sphase = $dcmon_SphaseWatts
+SET Tphase = $dcmon_TphaseWatts
+END
+VALUESEOF
 	return 0
 }
